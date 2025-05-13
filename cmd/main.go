39199d2c79c7eee3ae006a34178c739bbfd1b533
Gylmynnn <muhamadgilman999@gmail.com>
2025-05-13 11:18:59 +0800

    feat : homepage template handler

--- cmd/main.go
+++ cmd/main.go
@@ -13,5 +13,20 @@ func main() {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "templates/index.html")
+	if r.URL.Path != "/" {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
 
+	if r.Method != "GET" {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
 }
+
+
+
+
+
+
+
